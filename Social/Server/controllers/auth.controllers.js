7cfc740bbdb8be17b4dc7a19e5fcd26a29d887a4
Durgaprasad-Developer <bdurga.24bcs10012@sst.scaler.com>
2025-09-01 20:13:34 +0530

    auth done

--- Social/Server/controllers/auth.controllers.js
+++ Social/Server/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js'
-
-const signUp = async (req, res) => {
+import bcrypt from 'bcryptjs'
+export const signUp = async (req, res) => {
     const {name, userName, email, password} = req.body;
     console.log(req.body);
 
@@ -19,7 +19,13 @@ const signUp = async (req, res) => {
         return res.status(400).json({message: "Username already exists"});
     }
 
-    await User.create({name, userName, email, password});
+    if(password.length<6) return res.status(404).send("password should be > 6")
+
+    const salt = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(password, salt);
+    console.log(salt)
+
+    await User.create({name, userName, email, password: hashedPassword});
     res.status(201).json({message: "User created successfully"});
 
 }catch(err){
--- Social/Server/index.js
+++ Social/Server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 dotenv.config()
+import authRouter from './routes/auth.routes.js'
 
 const app = express();
 const PORT = 8000;
@@ -9,6 +10,10 @@ const PORT = 8000;
 connectDB()
 
 app.use(express.json());
+
+
+// Authentication routes
+app.use("/api/auth", authRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
